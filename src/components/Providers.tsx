61142dae19c8dbe05bf7d3eb3522dea7d49d1bf6
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-09-16 23:10:48 +0700

    refactor: wrap LanguageProvider with ThemeProvider in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,14 @@
 'use client';
+
+import { ThemeProvider } from '@/lib/ThemeContext';
 import { LanguageProvider } from '@/lib/LanguageContext';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <LanguageProvider>{children}</LanguageProvider>;
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        {children}
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
